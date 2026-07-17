3f22c80e0c33efadb8994a289d69943d234ad073
nestorka <nestorgg6@gmail.com>
2026-07-17 21:53:41 +0200

    Cpu updated

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::fs;
 
 pub struct Cpu {
@@ -60,12 +61,22 @@ impl Cpu {
             0x8000..=0x8FFF => {
                 let last_nibble = opcode & 0x0F;
                 match last_nibble {
-                    0x0 => self.op_ld(opcode),
+                    0x0 => self.op_ld_regs(opcode),
                     0x1 => self.op_or(opcode),
                     0x2 => self.op_and(opcode),
+                    0x3 => self.op_xor(opcode),
+                    0x4 => self.op_add_regs(opcode),
+                    0x5 => self.op_sub(opcode),
+                    0x6 => self.op_shr(opcode),
+                    0x7 => self.op_subn(opcode),
+                    0xE => self.op_shl(opcode),
                     _ => eprintln!("Unknown opcode: {:04X}", opcode),
                 }
             }
+            0x9000..=0x9FFF => self.op_sne_regs(opcode),
+            0xA000..=0xAFFF => self.op_ldi(opcode),
+            0xB000..=0xBFFF => self.op_jp_v0(opcode),
+            0xC000..=0xCFFF => self.op_rnd(opcode),
             _ => eprint!("Unknown opcode : {:04X}", opcode),
         }
     }
@@ -126,6 +137,12 @@ impl Cpu {
         self.registers[x as usize] = kk as u8;
     }
 
+    fn op_ld_regs(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+        let y = (opcode >> 4) & 0x0F;
+        self.registers[x as usize] = self.registers[y as usize];
+    }
+
     fn op_or(&mut self, opcode: u16) {
         let x = (opcode >> 8) & 0x0F;
         let y = (opcode >> 4) & 0x0F;
@@ -138,5 +155,81 @@ impl Cpu {
         let y = (opcode >> 4) & 0x0F;
 
         self.registers[x as usize] &= self.registers[y as usize];
+    }
+
+    fn op_xor(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+        let y = (opcode >> 4) & 0x0F;
+
+        self.registers[x as usize] ^= self.registers[y as usize];
+    }
+
+    fn op_add_regs(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+        let y = (opcode >> 4) & 0x0F;
+
+        let (result, overflow) =
+            self.registers[x as usize].overflowing_add(self.registers[y as usize]);
+        self.registers[x as usize] = result;
+        self.registers[0xF] = overflow as u8;
+    }
+
+    fn op_sub(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+        let y = (opcode >> 4) & 0x0F;
+
+        let (result, overflow) =
+            self.registers[x as usize].overflowing_sub(self.registers[y as usize]);
+        self.registers[x as usize] = result;
+        self.registers[0xF] = !overflow as u8;
+    }
+
+    fn op_shr(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+
+        self.registers[0xF] = self.registers[x as usize] & 0x1;
+        self.registers[x as usize] >>= 1;
+    }
+
+    fn op_subn(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+        let y = (opcode >> 4) & 0x0F;
+
+        let (result, overflow) =
+            self.registers[y as usize].overflowing_sub(self.registers[x as usize]);
+        self.registers[x as usize] = result;
+        self.registers[0xF] = !overflow as u8;
+    }
+
+    fn op_shl(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+        self.registers[0xF] = (self.registers[x as usize] >> 7) & 0x1;
+        self.registers[x as usize] <<= 1;
+    }
+
+    fn op_sne_regs(&mut self, opcode: u16) {
+        let x = (opcode >> 8) & 0x0F;
+        let y = (opcode >> 4) & 0x0F;
+
+        if self.registers[x as usize] != self.registers[y as usize] {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_ldi(&mut self, opcode: u16) {
+        self.index_register = opcode & 0x0FFF;
+    }
+
+    fn op_jp_v0(&mut self, opcode: u16) {
+        self.program_counter = (opcode & 0x0FFF) + self.registers[0] as u16;
+    }
+
+    fn op_rnd(&mut self, opcode: u16) {
+        let kk = opcode & 0xFF;
+        let x = (opcode >> 8) & 0x0F;
+
+        let random: u8 = rand::thread_rng().gen_range(0..=255);
+
+        self.registers[x as usize] = kk as u8 & random;
     }
 }
